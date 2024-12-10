7f852be0a6f2e284498730a0d6a345717f0dadf1
freyder_montano <fremontano24@gmail.com>
2024-12-10 12:06:46 -0300

    Mejorada la lógica de validación del token y manejo de la nueva contraseña. Se muestra un mensaje adecuado tras el cambio exitoso de la contraseña.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Login } from './pages/Login';
 import { Registrar } from './pages/Registrar';
 import { OlvidePassword } from './pages/OlvidePassword';
 import { ConfirmarCuenta } from './pages/ConfirmarCuenta';
+import { NuevoPassword } from './pages/NuevoPassword';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route index element={<Login />} />
           <Route path='registrar' element={<Registrar />} />
           <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path="olvide-password/:token" element={<NuevoPassword />} />
           <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
